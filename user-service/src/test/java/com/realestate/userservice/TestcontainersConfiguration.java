c1ff60d102dae732e94abb00ae54fce1e8d82b66
Salah <bhurgrisalah@gmail.oom>
2025-12-21 08:16:51 +0500

    setup

---
+++ user-service/src/test/java/com/realestate/userservice/TestcontainersConfiguration.java
@@ -0,0 +1,17 @@
+package com.realestate.userservice;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer mysqlContainer() {
+        return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+    }
+}
--- user-service/src/test/java/com/realestate/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/realestate/userservice/UserServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.realestate.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class UserServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
